9b52965210fcf48aaf563d0d96502bcaf39c0349
tpaktop <tpaktopsp@gmail.com>
2023-02-18 16:48:49 +0000

    Revert "add logger"
    This reverts commit 8e6bc4729775a468672a67d8828c697bad14d2e3.

--- deps.ts
+++ deps.ts
@@ -1,3 +1,2 @@
 export { router } from "https://deno.land/x/rutt@0.0.14/mod.ts";
 export { serve } from "https://deno.land/std@0.177.0/http/server.ts";
-export { getLogger } from "https://deno.land/std@0.177.0/log/mod.ts";
